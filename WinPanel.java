bc1f0601568cba101b4afa476997e478de47bf76
Connor <connor.odst@yahoo.com>
2014-03-18 21:49:50 -0500

    Jesus loves you

--- WinPanel.java
+++ WinPanel.java
@@ -1,12 +1,12 @@
-package LightCycles;
+package panels;
 
 import javax.swing.*;
 import java.awt.*;
 
+@SuppressWarnings("serial")
 public class WinPanel extends JPanel {
 
     public WinPanel() {
-
         setPreferredSize(new Dimension(160, 300));
     }
 
